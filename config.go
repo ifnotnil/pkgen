348425118e6d525b67936567cecdaa4262690a5c
Kostas Stamatakis <moukoublen@gmail.com>
2025-08-11 01:26:36 +0300

    fix

--- config.go
+++ config.go
@@ -20,7 +20,7 @@ var DefaultConfig = Config{
 	Templates: templates.TemplateConfigs{},
 
 	Generate: GenerateConfig{
-		OutputFile:    defaultNameFMT,
+		OutputFile:    "",
 		OutputFileMod: 0o644,
 	},
 }
