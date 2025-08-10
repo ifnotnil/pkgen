0db725807db40ed178432d8eb9206eced290b21e
Kostas Stamatakis <moukoublen@gmail.com>
2025-08-11 01:03:01 +0300

    logging and fixes

--- cmd/pkgen/main.go
+++ cmd/pkgen/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
+	"log/slog"
 	"os"
 	"path/filepath"
 
@@ -16,28 +16,41 @@ import (
 func main() {
 	ctx := context.Background()
 
+	loggerLevel := &slog.LevelVar{}
+	loggerLevel.Set(slog.LevelInfo)
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: false, Level: loggerLevel}))
+	slog.SetDefault(logger)
+
 	var (
 		config         string
 		template       string
 		templateCustom string
+		verbose        bool
 	)
 
 	flag.StringVar(&config, "config", ".pkgen.yml", "configuration file to use")
 	flag.StringVar(&config, "c", ".pkgen.yml", "configuration file to use")
 	flag.StringVar(&template, "template", "", "template to generate")
 	flag.StringVar(&templateCustom, "template-custom", "", "template to generate")
+	flag.BoolVar(&verbose, "verbose", false, "verbose output")
 	flag.Parse()
 
-	cnf := pkgen.Config{}
+	if verbose {
+		loggerLevel.Set(slog.LevelDebug)
+	}
+
+	logger.DebugContext(ctx, "cli arguments", slog.String("config", config), slog.String("template", template), slog.String("template-custom", templateCustom))
+
+	cnf := pkgen.DefaultConfig
 
 	if runningInsideGoGenerate() {
 		// if it is running inside go:generate query only the local package.
 		cnf.PackagesQuery.Patterns = []string{"."}
-		fmt.Printf("RUNNING INSIRE GO:GENERATE\n\n\n")
+		logger.DebugContext(ctx, "running inside a go:generate")
 	} else {
-		err := parseConfig(config, &cnf)
+		err := parseConfig(ctx, config, &cnf)
 		if err != nil {
-			fmt.Printf("error while parsing config: %s", err.Error())
+			logger.ErrorContext(ctx, "error while parsing config", slog.String("error", err.Error()))
 			os.Exit(1)
 		}
 	}
@@ -50,20 +63,27 @@ func main() {
 		cnf.Templates = append(cnf.Templates, templates.TemplateConfig{CustomTemplateFile: templateCustom})
 	}
 
+	logger.DebugContext(ctx, "config", slog.Any("config", cnf))
+
 	packages, err := pkgen.Packages(ctx, cnf.PackagesQuery)
 	if err != nil {
-		fmt.Printf("error while querying packages: %s", err.Error())
+		logger.ErrorContext(ctx, "error while querying packages", slog.String("error", err.Error()))
 		os.Exit(1)
 	}
+
+	logger.DebugContext(ctx, "packages", slog.Any("packages", packages))
 
 	tmps, err := templates.GetTemplates(cnf.Templates)
 	if err != nil {
-		fmt.Printf("error while querying packages: %s", err.Error())
+		logger.ErrorContext(ctx, "error while getting templates", slog.String("error", err.Error()))
 		os.Exit(1)
 	}
+	logger.DebugContext(ctx, "packages", slog.Any("packages", packages))
 
+	logger.DebugContext(ctx, "generating", slog.Int("packages", len(packages)), slog.Int("templates", len(tmps)))
 	for _, p := range packages {
 		for _, tmp := range tmps {
+			logger.DebugContext(ctx, "generating", slog.String("package", p.Name), slog.String("template", tmp.Name()))
 			err = pkgen.GenerateInPackage(ctx, p, tmp, pkgen.GenerateConfig{})
 			if err != nil {
 				fmt.Printf("error while generating: %s", err.Error())
@@ -79,20 +99,18 @@ func runningInsideGoGenerate() bool {
 	return exists
 }
 
-func fileExists(filename string) bool {
-	_, err := os.Stat(filename)
-	return !errors.Is(err, os.ErrNotExist)
-}
-
-func parseConfig(filePath string, cnf *pkgen.Config) error {
+func parseConfig(ctx context.Context, filePath string, cnf *pkgen.Config) error {
 	filePath = filepath.Clean(filePath)
-	if fileExists(filePath) {
-		if b, err := os.ReadFile(filePath); err == nil {
-			err = yaml.Unmarshal(b, &cnf)
-			if err != nil {
-				fmt.Printf("error while parsing config yaml: %s", err.Error())
-				os.Exit(1)
-			}
+
+	_, err := os.Stat(filePath)
+	if err != nil {
+		return err
+	}
+
+	if b, err := os.ReadFile(filePath); err == nil {
+		err = yaml.Unmarshal(b, &cnf)
+		if err != nil {
+			return err
 		}
 	}
 
--- config.go
+++ config.go
@@ -7,3 +7,20 @@ type Config struct {
 	Templates     templates.TemplateConfigs `yaml:"templates"`
 	Generate      GenerateConfig            `yaml:"generate"`
 }
+
+var DefaultConfig = Config{
+	PackagesQuery: PackagesQueryConfig{
+		IncludeTests: false,
+		Env:          nil,
+		BuildFlags:   nil,
+		Dir:          "",
+		Patterns:     []string{"./..."},
+	},
+
+	Templates: templates.TemplateConfigs{},
+
+	Generate: GenerateConfig{
+		OutputFile:    defaultNameFMT,
+		OutputFileMod: 0o644,
+	},
+}
